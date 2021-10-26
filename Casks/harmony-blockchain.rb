1e1fdceafea63b2923a1d590abdaab0565fca54b
omhmichaels <omhmichaels@gmail.com>
2021-10-26 00:19:58 -0500

    Broken URL Error in harmony-blockchain.rb

--- Casks/harmony-blockchain.rb
+++ Casks/harmony-blockchain.rb
@@ -2,7 +2,7 @@ cask "harmony-blockchain" do
 
     version "4.3.0"
     sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
-    url "https://github.com/harmony-blockchain/harmony/releases/download/v#{version}/harmony-macos-#{version}.zip"
+    url "https://github.com/harmony-one/harmony/releases/download/v4.3.0/harmony-macos-4.3.0.zip"
     name "Harmony One Blockchain Node"
     desc "Network node for the Harmony blockchain"
     homepage "https://github.com/harmony-blockchain/harmony"

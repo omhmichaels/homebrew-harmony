c2f81c739b6fdd04037a9c292cf814e14b6e1c34
omhmichaels <omhmichaels@gmail.com>
2021-11-04 20:44:55 -0600

    Fix Formula/harmony-one

--- Formula/harmony-one.rb
+++ Formula/harmony-one.rb
@@ -2,16 +2,16 @@ class HarmonyOne < Formula
     desc "Harmony One Blockchain Node"
     homepage "https://github.com/harmony-one/harmony"
     # CONTRIBUTORS: Update the following (url, sha256)
-    url "https://github.com/harmony-one/harmony/archive/refs/tags/v#{version}.tar.gz"
+    url "https://github.com/harmony-one/harmony/archive/refs/tags/v4.3.0.zip"
     sha256 "d463cd60973509f2a7e906582a09a7cd21cf3638b956f164c63e0536a2063367"
     license "LGPL-3.0-or-later"
 
     # CONTRIBUTORS: Versions may need to be updated according to documentation at:
     #    -  https://github.com/harmony-one/harmony 
-    depends_on "go@1.16.3"
+    depends_on "go@1.16.3" => [:build, :test]
     depends_on "openssl@1.1" 
     depends_on "gmp"
-    depends_on "make" 
+    depends_on "make" => [:build, :test]
     depends_on "jq"
     depends_on "bash" 
 

0b922f4e32509fb5690c8676303b5dd4a093609a
omhmichaels <omhmichaels@gmail.com>
2021-11-05 16:34:28 -0600

    Add Cask Missing after merge error

--- Casks/harmony-blockchain.rb
+++ Casks/harmony-blockchain.rb
@@ -2,32 +2,33 @@ cask "harmony-blockchain" do
 
     version "4.3.0"
     sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
-    url "https://github.com/harmony-one/harmony/releases/download/v#{version}/harmony-macos-#{version}.zip"
+    url "https://github.com/harmony-one/harmony/releases/download/v4.3.0/harmony-macos-4.3.0.zip"
     name "Harmony One Blockchain Node"
     desc "Network node for the Harmony blockchain"
-    homepage "https://github.com/harmony-one/harmony"
+    homepage "https://github.com/harmony-blockchain/harmony"
 
+    depends_on macos:  ">= :high_sierra"
 
     # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
     # Attempt to correctly linking these dylibs. 
     # Guidance appreciated if theres is a better method.
     shimscript = "#{staged_path}/harmony-shim.sh"
-    binary shimscript, target: "#{HOMEBREW_PREFIX}/bin/harmony-one"
+    binary shimscript, target: "#{HOMEBREW_PREFIX}/bin/harmony"
     # Copy artifacts to the Application Directory
-    artifact "./", target: "#{HOMEBREW_PREFIX}/harmony-one"
+    artifact "./", target: "#{HOMEBREW_PREFIX}/harmony-blockchain"
     
 
     preflight do
         File.write shimscript, <<~EOS
             #!/bin/sh
-            cd /usr/local/harmony-one/
+            cd /usr/local/harmony-blockchain/
             ./harmony
         EOS
     end
 
 
     # Delete application directory created    
-    uninstall delete: ["#{HOMEBREW_PREFIX}/harmony-one"]
+    uninstall delete: ["#{HOMEBREW_PREFIX}/harmony-blockchain"]
 
     # Verify the unsigned binary as well as each of the dylib libraries.  
     # After validation the following error will occcurs

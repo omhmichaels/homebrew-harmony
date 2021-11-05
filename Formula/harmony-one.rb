43a3d42eb59dc7cd2d71bcdf36bc8f1eeab981af
omhmichaels <omhmichaels@gmail.com>
2021-11-04 21:54:04 -0600

    Fix Formula/harmony-one

--- Formula/harmony-one.rb
+++ Formula/harmony-one.rb
@@ -2,53 +2,15 @@ class HarmonyOne < Formula
     desc "Harmony One Blockchain Node"
     homepage "https://github.com/harmony-one/harmony"
     # CONTRIBUTORS: Update the following (url, sha256)
-    url "https://github.com/harmony-one/harmony/archive/refs/tags/v4.3.0.zip"
-    sha256 "9868535e36a56589dbeefd576ad4bb8ab1aa87b40b7b436353d0ea7730299cbc"
+    url "https://github.com/harmony-one/harmony/releases/download/v4.3.0/harmony-macos-4.3.0.zip"
+    sha256 "a8fd1a63f46886d51605c71fef15a7dabe5509dd35ce71ce5df2cc9ccad7860d"
     license "LGPL-3.0-or-later"
 
-    # CONTRIBUTORS: Versions may need to be updated according to documentation at:
-    #    -  https://github.com/harmony-one/harmony 
-    depends_on "go@1.16" => [:build, :test]
-    depends_on "openssl@1.1" 
-    depends_on "gmp"
-    depends_on "make" => [:build, :test]
-    depends_on "jq"
-    depends_on "bash" 
-
-  
-    # CONTRIBUTORS: Keep resources updated according to the changelog release tag:
-    #     - https://github.com/harmony-one/harmony/releases/tag/v#{version}
-    resource "bls" do
-      url "https://github.com/harmony-one/bls/archive/refs/tags/v0.0.6.tar.gz",
-        version: "0.0.6"
-    end
-
-  
     def install
-      ENV.O0 if OS.linux?
-      ENV.deparallelize
-
-      # Get unversioned source for custom harmony bls fork
-      # Adhoc method of adding resource due to errors with resource formula method
-      # needing a version. 
-      system "git clone https://github.com/harmony-one/mcl.git"
-
-      # Install Build Tools
-      system "scripts/install_build_tools.sh"
-
-      # Correct Directory Locations In Build Scripts
-      # TODO: Switch to brew 'inreplace' formula method
-      system "sed -i -e 's/openssl/openssl\@1\.1/g' Makefile"
-      system "sed -i -e 's/openssl/openssl\@1\.1/g' scripts/go_executable_build.sh" 
-
-      # Build Binarys
-      system "go mod tidy"
-      system "make "
-
       # Move Binaries and Libs to correct Install Locations
       bin.install "bin/harmony"
       bin.install "bin/bootnode"
-      lib.install  Dir["bin/*dylib"] 
+      lib.install  Dir["dylib"] 
 
     end
     
